aaacea49f082aabdc1953c115a13a301a9235563
Mamadou Diagne <genova@dofbi.com>
2022-08-06 13:29:43 +0000

    ♻️ refactor(Parser): add User-Agent

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,9 @@ import nunjucks from 'nunjucks'
 // @ts-ignore
 import dateFilter from 'nunjucks-date-filter'
 
-const parser = new Parser()
+const parser = new Parser({
+  headers: {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'},
+})
 const env = nunjucks.configure({ autoescape: false })
 env.addFilter('date', dateFilter)
 
